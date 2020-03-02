9182c5abef2134ba24a8810798ca4df5325d8570
unknown <baliighadaa@gmail.com>
2020-03-02 17:05:07 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ClientComponent } from './components/client/client.component';
+import { InstructionComponent } from './components/instruction/instruction.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
+import { DetailsComponent } from './components/details/details.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ClientComponent,
+    InstructionComponent,
+    TransactionComponent,
+    DetailsComponent,
+    FooterComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
